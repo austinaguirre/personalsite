2f5339a03b207899ed98717395cbf6a8ebf6d5a6
Austin Aguirre <austinaguirre@Austins-MacBook-Air.local>
2024-02-05 14:27:16 -0500

    pages

---
+++ src/pages/NoPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NoPage = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! The page you're looking for doesn't exist.</p>
+      {/* You can add a link back to the home page or any other relevant content */}
+    </div>
+  );
+};
+
+export default NoPage;
