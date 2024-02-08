24ec53ab186d966ec097b9aef97973f4324f4ea4
Austin Aguirre <austinaguirre@Austins-MacBook-Air.local>
2024-02-08 14:38:36 -0500

    curr

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@
 
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,10 +8,10 @@ const Home = () => {
       {/* Add more content or components as needed */}
 
       <audio controls>
-        <source src="https://github.com/austinaguirre/personalsite/blob/main/public/music/Derp-Song%20(2).wav" type="audio/wav" />
+        <source src="public/smusic/song1.wav" type="audio/wav" />
         Your browser does not support the audio element.
       </audio>
-
+ 
     </div>
   );
 };
